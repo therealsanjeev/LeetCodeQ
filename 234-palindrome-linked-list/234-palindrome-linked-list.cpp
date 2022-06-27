c40c32c3e396c9f06668f119de060a452909d3e5
Sanjeev Gupta <therealsanjeev0@gmail.com>
2022-06-28 00:39:44 +0530

    Time: 362 ms (41.52%), Space: 123.2 MB (26.57%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,52 +1,31 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         
-        if(head==NULL)
-        return true;
-    int c=0;
-    ListNode *temp = head;
-    // count the length of linkedlist
-    while(temp != NULL){        
-        c++;
-        temp = temp ->next;
-    }
-    
-    // break the linkedlist in two half
-    int mid = (c-1)/2;
-    int i=0;
-    ListNode * head2= head;
-    while(i<mid){
-        head2=head2->next;
-        i++;
-    }
-    temp = head2->next ;
-    head2->next = NULL;
-    head2= temp;
-    
-    // reverse the second half of the linkedlist
-    ListNode *pre = NULL;    
-    ListNode *next = NULL;
-    ListNode *current = head2;
-    while(current != NULL){
-        next = current->next;
-        current ->next = pre;
-        pre= current ;
-        current = next;
-    }
-    head2 = pre;
-    
-    // compare the second half linkedlist to first half linkedlis
-    ListNode *temp1=head;
-    ListNode *temp2 = head2;
-    while( temp2 != NULL){
-        if(temp1->val != temp2->val){
-            return false;
+        stack<int> st;
+        ListNode* tp=head;
+        while(tp){
+            st.push(tp->val);
+            tp=tp->next;
         }
-        temp1=temp1->next;
-        temp2=temp2->next;
-    }
-    return true; 
+        tp=head;
+        while(tp){
+            if(st.top()!=tp->val){
+                return false;
+            }
+            st.pop();
+            tp=tp->next;
+        }
+        return true;
     }
 };

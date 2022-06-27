b93e468304a26c0e7f7ffd6e797b699bd349701f
Sanjeev Gupta <therealsanjeev0@gmail.com>
2022-06-28 00:16:34 +0530

    Time: 6 ms (83.23%), Space: 6.7 MB (62.65%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -6,6 +6,8 @@ public:
         if(num1=="0"||num2=="0") return "0";
         if(num1=="1") return num2;
         if(num2=="1")return num1;
+        
+        
         string ans="";
         
         vector<int> v(num1.size()+num2.size(),0);

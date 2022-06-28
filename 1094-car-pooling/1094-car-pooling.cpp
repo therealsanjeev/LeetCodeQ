205af855993b1fb7af92958dd73a610e8310d491
Sanjeev Gupta <therealsanjeev0@gmail.com>
2022-06-29 01:06:44 +0530

    Time: 25 ms (42.70%), Space: 10.6 MB (52.19%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,25 +1,36 @@
 class Solution {
-    static bool comp(vector<int> a,vector<int> b){
-        return a[1]<b[1];
-    }
+    // static bool comp(vector<int> a,vector<int> b){
+    //     return a[1]<b[1];
+    // }
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         
-        sort(trips.begin(),trips.end(),comp);
-        priority_queue<pair<int,int>> pq;
+        vector<int> v(1001,0);
         for(auto it:trips){
-            pq.push({-1*it[2],it[0]});
+            v[it[1]]+=it[0];
+            v[it[2]]-=it[0];
+            
         }
-        int curr=0;
-        for(auto it:trips){
-            curr+=it[0];
-            while(it[1]>=(-1*pq.top().first)){
-                curr-=pq.top().second;
-                pq.pop();
-            }
-            if(curr>capacity) 
-                return false;
+        int t=0;
+        for(auto it:v){
+            t+=it;
+            if(t>capacity) return false;
         }
+        // sort(trips.begin(),trips.end(),comp);
+        // priority_queue<pair<int,int>> pq;
+        // for(auto it:trips){
+        //     pq.push({-1*it[2],it[0]});
+        // }
+        // int curr=0;
+        // for(auto it:trips){
+        //     curr+=it[0];
+        //     while(it[1]>=(-1*pq.top().first)){
+        //         curr-=pq.top().second;
+        //         pq.pop();
+        //     }
+        //     if(curr>capacity) 
+        //         return false;
+        // }
         
         return true;
     }

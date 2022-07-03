690652f13837c3788b4457ee8fe67d467d11e301
Sanjeev Gupta <therealsanjeev0@gmail.com>
2022-07-03 23:34:51 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (25.86%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.go
@@ -0,0 +1,24 @@
+func wiggleMaxLength(nums []int) int {
+    
+    if len(nums) < 2 {
+         return len(nums)
+    }
+    
+    var prevdiff=nums[1]-nums[0]
+    
+    count := 2
+    if prevdiff== 0{
+        count = 1
+    }
+    
+    for i:=2; i<len(nums);i++{
+        diff:=nums[i]-nums[i-1]
+        if(diff>0 && prevdiff<=0) || (diff<0 && prevdiff>=0) {
+            count++
+            prevdiff=diff
+        }
+    }
+    
+    
+    return count
+}
